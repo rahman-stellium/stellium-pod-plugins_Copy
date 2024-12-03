94129ed2fc958f7e8239123ed7b7acf50081213f
=Shad Musthafa <=shad.musthafa@stellium.com>
2024-12-04 00:55:27 +0530

    Add quantity confirmation table

--- podplugins/webapp/confirmationPlugin/controller/PluginView.controller.js
+++ podplugins/webapp/confirmationPlugin/controller/PluginView.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-  ['sap/ui/model/json/JSONModel', 'sap/dm/dme/podfoundation/controller/PluginViewController', 'sap/base/Log'],
-  function(JSONModel, PluginViewController, Log) {
+  [
+    'sap/ui/model/json/JSONModel',
+    'sap/dm/dme/podfoundation/controller/PluginViewController',
+    'sap/base/Log',
+    'sap/ui/core/format/NumberFormat'
+  ],
+  function(JSONModel, PluginViewController, Log, NumberFormat) {
     'use strict';
 
     var oLogger = Log.getLogger('confirmationPlugin', Log.Level.INFO);
@@ -16,21 +21,132 @@ sap.ui.define(
           if (PluginViewController.prototype.onInit) {
             PluginViewController.prototype.onInit.apply(this, arguments);
           }
+
+          this.getView().setModel(new JSONModel({ value: [] }), 'quantitiesModel');
         },
 
         /**
          * @see PluginViewController.onBeforeRenderingPlugin()
          */
         onBeforeRenderingPlugin: function() {
-
+          this.subscribe('phaseSelectionEvent', this.getQuantityConfirmationData, this);
+          this.publish('requestForPhaseData', this);
+          // this.podType = this.getPodSelectionModel().getPodType();
+          // //Work Center POD event for Prodcuction Order
+          // if (this.getPodSelectionModel().getPodType() === 'WORK_CENTER') {
+          //     this.subscribe('OperationListSelectEvent', this.onOperationSelected, this);
+          //     // this.publish("phaseSelectionEventWIList", this);
+          // }
+          // //Order POD event for Process Order
+          // if (this.getPodSelectionModel().getPodType() === 'ORDER') {
+          //     this.subscribe('phaseSelectionEvent', this.onPhaseSelected, this);
+          //     this.publish('requestForPhaseData', this);
+          // }
         },
 
-        onExit: function() {
-        },
+        onExit: function() {},
 
         onBeforeRendering: function() {},
 
         onAfterRendering: function() {},
+
+        getQuantityConfirmationData: function(sChannelId, sEventId, oData) {
+          var oPodSelectionModel = this.getPodSelectionModel();
+          this.plant = this.getPodController().getUserPlant();
+          this.resource = oPodSelectionModel.workCenter;
+
+          if (!oPodSelectionModel || !oPodSelectionModel.timeZoneId) {
+            // this.createMessage('missingInformation', MessageType.Error);
+            return false;
+          }
+
+          this.plantTimeZoneId = oPodSelectionModel.timeZoneId;
+
+          this.selectedOrderData = oData;
+          this.getQuantityConfirmationSummary(this.selectedOrderData);
+        },
+
+        getQuantityConfirmationSummary: function(oData) {
+          var productionUrl = this.getProductionDataSourceUri();
+          var oParameters = {};
+          oParameters.shopOrder = oData.selectedShopOrder;
+          oParameters.batchId = oData.selectedSfc;
+          if (oData.orderSelectionType === 'PROCESS') {
+            oParameters.phase = oData.phaseId;
+          } else if (oData.orderSelectionType === 'PRODUCTION') {
+            oParameters.phase = oData.operation.operation;
+          }
+          this.oParameters = oParameters;
+          // this.onOpenViewQuantityReportDialog(true);
+          var sUrl = productionUrl + 'quantityConfirmation/summary';
+          this.postFetchQuantityConfirmationData(sUrl, oParameters);
+        },
+
+        postFetchQuantityConfirmationData: function(sUrl, oParameters) {
+          var that = this;
+          var userAuthFlag = this.selectedOrderData.userAuthorizedForWorkCenter;
+          var status = this.selectedOrderData.status;
+
+          var oTable = that.byId('quantityConfirmationTable');
+
+          //Set table busy indicators
+          // that.byId('quantityConfirmationTable') &&
+          //     that.byId('quantityConfirmationTable').setBusyIndicatorDelay(0);
+          // that.byId('quantityConfirmationTable') && that.byId('quantityConfirmationTable').setBusy(true);
+
+          if (oTable) {
+            oTable.setBusyIndicatorDelay(0);
+            oTable.setBusy(true);
+          }
+
+          that.ajaxGetRequest(
+            sUrl,
+            oParameters,
+            function(oResponseData) {
+              // adding user work center authorization flag to response
+              oResponseData.userAuthorizedForWorkCenter =
+                userAuthFlag !== null && userAuthFlag !== undefined ? userAuthFlag : false;
+              oResponseData.status = status;
+              that.quantityConfirmationList = oResponseData;
+              var temp = [];
+              temp.push(that.quantityConfirmationList);
+              var quantityConfirmationOverviewModel = new JSONModel();
+              quantityConfirmationOverviewModel.setData(temp);
+              that.getView().getModel('quantitiesModel').setProperty('/value', temp);
+
+              // that.byId('quantityConfirmationTable') &&
+              //     that.byId('quantityConfirmationTable').setModel(quantityConfirmationOverviewModel);
+              // that.byId('quantityConfirmationTable') &&
+              //     that.byId('quantityConfirmationTable').setBusy(false);
+
+              if (oTable) {
+                oTable.setBusy(false);
+              }
+            },
+            function(oError, oHttpErrorMessage) {
+              var err = oError ? oError : oHttpErrorMessage;
+              that.showErrorMessage(err, true, true);
+              that.quantityConfirmationList = {};
+              that.byId('quantityConfirmationTable').setBusy(false);
+            }
+          );
+        },
+
+        showValueWithUom: function(sValue, sUOM) {
+          if (!sValue) {
+            return '0';
+          }
+
+          let oIntNumberFormat = NumberFormat.getIntegerInstance({
+            groupingEnabled: true,
+            style: 'standard',
+            parseAsString: true
+          });
+
+          var iFormattedValue = oIntNumberFormat.format(sValue.toString());
+
+          return sUOM ? `${iFormattedValue} ${sUOM}` : iFormattedValue;
+        }
       }
     );
 
