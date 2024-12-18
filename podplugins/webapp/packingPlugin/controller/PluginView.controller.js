337561f4e68337f1446d319e49378d3666a56cfd
=Shad Musthafa <=shad.musthafa@stellium.com>
2024-12-18 12:14:55 +0530

    Created the view and basic controller

--- podplugins/webapp/packingPlugin/controller/PluginView.controller.js
+++ podplugins/webapp/packingPlugin/controller/PluginView.controller.js
@@ -1,36 +1,51 @@
-sap.ui.define(['sap/ui/model/json/JSONModel', 'sap/dm/dme/podfoundation/controller/PluginViewController'], function(
-  JSONModel,
-  PluginViewController
-) {
-  'use strict';
+sap.ui.define(
+  [
+    'sap/dm/dme/podfoundation/controller/PluginViewController',
+    'sap/ui/model/json/JSONModel',
+    'sap/base/Log',
+    '../util/PackingPluginFormatter',
+    'sap/dm/dme/formatter/GeneralFormatter',
+    'sap/dm/dme/formatter/StatusFormatter'
+  ],
+  function(PluginViewController, JSONModel, Log, PackingPluginFormatter, GeneralFormatter, StatusFormatter) {
+    'use strict';
 
-  var oLogger = Log.getLogger('packingPlugin', Log.Level.INFO);
+    var oLogger = Log.getLogger('packingPlugin', Log.Level.INFO);
 
-  var oPluginViewController = PluginViewController.extend(
-    'stellium.ext.podplugins.packingPlugin.controller.PluginView',
-    {
-      onInit: function() {
-        if (PluginViewController.prototype.onInit) {
-          PluginViewController.prototype.onInit.apply(this, arguments);
-        }
-      },
+    var oPluginViewController = PluginViewController.extend(
+      'stellium.ext.podplugins.packingPlugin.controller.PluginView',
+      {
+        packingPluginFormatter: PackingPluginFormatter,
+        generalFormatter: GeneralFormatter,
+        statusFormatter: StatusFormatter,
 
-      /**
+        metadata: {
+          properties: {}
+        },
+
+        onInit: function() {
+          if (PluginViewController.prototype.onInit) {
+            PluginViewController.prototype.onInit.apply(this, arguments);
+          }
+        },
+
+        /**
          * @see PluginViewController.onBeforeRenderingPlugin()
          */
-      onBeforeRenderingPlugin: function() {},
+        onBeforeRenderingPlugin: function() {},
 
-      onExit: function() {},
+        onExit: function() {
+          if (PluginViewController.prototype.onExit) {
+            PluginViewController.prototype.onExit.apply(this, arguments);
+          }
+        },
 
-      onBeforeRendering: function() {},
+        onBeforeRendering: function() {},
 
-      onAfterRendering: function() {},
-
-      isSubscribingToNotifications: function() {
-        return true;
+        onAfterRendering: function() {}
       }
-    }
-  );
+    );
 
-  return oPluginViewController;
-});
+    return oPluginViewController;
+  }
+);
